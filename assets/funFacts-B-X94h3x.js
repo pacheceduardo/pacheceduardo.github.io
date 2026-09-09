f3953205b67974060a6f7798cc98f67d331fd171
pacheceduardo <pacheceduardo@users.noreply.github.com>
2026-09-09 18:51:13 +0000

    Deploy app 4Bros (via Actions do beachvolei4bros) pacheceduardo/beachvolei4bros@c013711d3b0a2ef2c4486aa0d46da234273fb611

--- assets/funFacts-B-X94h3x.js
+++
@@ -1,6 +0,0 @@
-import{c as e}from"./index-B3QTm5Bo.js";/**
- * @license lucide-react v0.462.0 - ISC
- *
- * This source code is licensed under the ISC license.
- * See the LICENSE file in the root directory of this source tree.
- */const r=e("UserRound",[["circle",{cx:"12",cy:"8",r:"5",key:"1hypcn"}],["path",{d:"M20 21a8 8 0 0 0-16 0",key:"rfgkzh"}]]),o=[{id:"aries",label:"Áries",emoji:"♈"},{id:"touro",label:"Touro",emoji:"♉"},{id:"gemeos",label:"Gêmeos",emoji:"♊"},{id:"cancer",label:"Câncer",emoji:"♋"},{id:"leao",label:"Leão",emoji:"♌"},{id:"virgem",label:"Virgem",emoji:"♍"},{id:"libra",label:"Libra",emoji:"♎"},{id:"escorpiao",label:"Escorpião",emoji:"♏"},{id:"sagitario",label:"Sagitário",emoji:"♐"},{id:"capricornio",label:"Capricórnio",emoji:"♑"},{id:"aquario",label:"Aquário",emoji:"♒"},{id:"peixes",label:"Peixes",emoji:"♓"}],s=Object.fromEntries(o.map(i=>[i.id,i])),a=[{id:"playstation",label:"PlayStation"},{id:"xbox",label:"Xbox"},{id:"switch",label:"Switch"},{id:"pc",label:"PC"},{id:"mobile",label:"Celular"}],c=Object.fromEntries(a.map(i=>[i.id,i])),b=200;export{c as C,b as M,r as U,s as Z,o as a,a as b};
