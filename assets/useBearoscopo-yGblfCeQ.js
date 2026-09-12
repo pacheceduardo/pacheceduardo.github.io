6dd76692b90131feda2f19a0d6ce3afbd1d41781
pacheceduardo <pacheceduardo@users.noreply.github.com>
2026-09-12 21:08:40 +0000

    Deploy app 4Bros (via Actions do beachvolei4bros) pacheceduardo/beachvolei4bros@78238d80414aaad2a6609e2e2b68f75a66a323c7

--- assets/useBearoscopo-yGblfCeQ.js
+++
@@ -1,1 +0,0 @@
-import{u as e}from"./query-BydKBSTv.js";import{s as a}from"./index--cNej59E.js";function u(){return e({queryKey:["bearoscopo"],staleTime:60*6e4,queryFn:async()=>{const{data:o,error:r}=await a.rpc("my_bearoscopo");if(r)throw r;return o??[]}})}export{u};
